63f960fc9b2e224b9d65ad3d53d20a9ab29c4912
lcnorine <60401135+lcnorine@users.noreply.github.com>
2020-04-29 22:53:22 -0700

    Began counting to and from the proper numbers in gameState

--- Piezas.cpp
+++ Piezas.cpp
@@ -105,7 +105,7 @@ Piece Piezas::gameState()
 	int lo = 0;
 	for (int i = 0; i < 3; i++)
 	{
-		for (int j = 1; j < 4; j++)
+		for (int j = 0; j < 4; j++)
 		{
 			if (board[i][j] == X)
 			{
@@ -143,7 +143,7 @@ Piece Piezas::gameState()
 	wx = 0;
 	for (int j = 0; j < 4; j++)
 	{
-		for (int i = 1; i < i; i++)
+		for (int i = 0; i < 3; i++)
 		{
 			if (board[i][j] == X)
 			{
